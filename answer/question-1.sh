e3690787e2c670b2e654feec9704f7488ea4b7ab
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-06-23 15:35:28 +0900

    不要なファイルを削除し、CKAD試験に向けた学習環境を整理しました。これにより、学習効率の向上が期待されます。

--- answer/question-1.sh
+++
@@ -1,1 +0,0 @@
-kubectl logs -n filter -l 'app in (pod-3, pod-7, pod-8)' > /etc/pods.log
--- answer/question-16.sh
+++
@@ -1,9 +0,0 @@
-helm repo add bitnami https://charts.bitnami.com/bitnami
-
-helm search repo bitnami/nginx
-
-helm install my-nginx bitnami/nginx --create-namespace -n ckad-helm
-
-helm list -n ckad-helm
-
-helm upgrade my-nginx bitnami/nginx -n ckad-helm --set replicaCount=2
